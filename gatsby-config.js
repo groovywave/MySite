7a9dbf9550261742163cff8598c7395ada0d6726
groove <vacation555@gmail.com>
2022-04-17 01:06:56 +0900

    20220416

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,17 +10,33 @@ module.exports = {
     title: `ESSENTIALS`,
     description: `おいしい食材と食事を探求するサイト`,
     lang: `ja`,
+    siteUrl: `https://shiny-parfait-d3e8b1.netlify.app/`,
+    locale: `ja_JP`,
+    fbappid: `360203250666702`,
   },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
-      options:{
+      options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ESSENTIALS エッセンシャルズ`,
+        short_name: `ESSENTIALS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#477294`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
